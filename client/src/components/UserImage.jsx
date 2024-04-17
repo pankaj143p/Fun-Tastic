ff11419aa6c4a0878b9c0f25d5b147fbd1b53d39
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2024-04-17 15:22:21 +0530

    add all

---
+++ client/src/components/UserImage.jsx
@@ -0,0 +1,18 @@
+import { Box } from "@mui/material";
+// import { API_BASE_URL } from "config";
+
+const UserImage = ({ image, size = "60px" }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={image}
+      />
+    </Box>
+  );
+};
+
+export default UserImage;
---
+++ server/controllers/posts.js
@@ -0,0 +1,75 @@
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+
+/* CREATE */
+export const createPost = async (req, res) => {
+  try {
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+    const newPost = new Post({
+      userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      like: {},
+      comments: [],
+    });
+    await newPost.save();
+
+    // const post = await Post.find();
+    const post = await Post.find().sort({ createdAt: -1 }); //query to sort the posts by their creation date in descending order
+    console.log(post);
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+/* READ */
+export const getFeedPosts = async (req, res) => {
+  try {
+    // const post = await Post.find();
+    const post = await Post.find().sort({ createdAt: -1 });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    // const post = await Post.find({ userId });
+    const post = await Post.find({ userId }).sort({ createdAt: -1 });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(409).json({ message: err.message });
+  }
+};
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+
+    res.status(200).json(updatedPost);
+  } catch (err) {}
+};
